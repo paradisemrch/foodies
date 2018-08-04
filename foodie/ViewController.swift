c5c934b1fd85bc158d7aa491bdc089dbf5aa8aae
William Wang <william@williamdemacbook-pro-2.local>
2018-08-04 15:54:51 +0800

    Initial Commit

---
+++ foodie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  foodie
+//
+//  Created by William Wang on 2018/8/4.
+//  Copyright © 2018年 rebecca. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
